db37c1259c044838879761403fdd9607961efcb3
YSidorko <yury.sidorko@gmail.com>
2023-11-08 17:48:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### @jw/const
+*version 1.0.0*
+
+---
+Job worker common values
---
+++ index.js
@@ -0,0 +1,46 @@
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const WEEK = DAY * 7;
+
+const MAIN_DIR = process?.env.INIT_CWD || process?.env.PWD || '';
+const DEFAULT_LAYOUT = 'index';
+
+const STORE_TYPES = {
+  FS_STORE: 'fs',
+  MONGODB_STORE: 'mongodb',
+  SQLITE_STORE: 'sqlite'
+};
+Object.freeze(STORE_TYPES);
+
+const RESERVED_URLS = {
+  PAGE_LOGIN: '/login',
+  PAGE_LOGOUT: '/logout',
+  PAGE_ADMIN: '/admin',
+
+  MEMORY_ROUTE: '/__memory'
+};
+Object.freeze(RESERVED_URLS);
+
+const DATA_TYPES = {
+  TYPE_ENVIRONMENT: 'environment',
+  TYPE_WORKER: 'worker',
+  TYPE_LOG: 'log'
+};
+Object.freeze(DATA_TYPES);
+
+module.exports = {
+  SECOND,
+  MINUTE,
+  HOUR,
+  DAY,
+  WEEK,
+
+  ...DATA_TYPES,
+  ...STORE_TYPES,
+  ...RESERVED_URLS,
+
+  MAIN_DIR,
+  DEFAULT_LAYOUT
+};
